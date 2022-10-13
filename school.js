d7ea23d0c01aef6eee184213a9ed804a63bf42fe
atomic-ustadh <110829892+atomic-ustadh@users.noreply.github.com>
2022-10-13 01:36:29 +0100

    Commit Group 1.0 Tajweed Project

---
+++ school.js
@@ -0,0 +1,9 @@
+function HideNav(){
+    document.getElementById('mobile-nav').style.display = "flex";
+    document.getElementById('closebtn').style.display = "none";
+}
+
+function OpenNav(){
+    document.getElementById('mobile-nav').style.display = "none";
+    document.getElementById('closebtn').style.display = "block";
+}
